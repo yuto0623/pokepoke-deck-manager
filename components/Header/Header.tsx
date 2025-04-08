a22a9c3fb4239defbd15d58052530728e81f8f36
yuto <yuto.ryr0623@gmail.com>
2025-04-08 21:08:53 +0900

    Fix header title typo from "ポケカ" to "ポケポケ"

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header className="neumorphism p-4">
-      <h1 className="text-2xl font-bold">ポケカ デッキビルダー</h1>
+      <h1 className="text-2xl font-bold">ポケポケ デッキビルダー</h1>
       <p className="mt-2">あなたのデッキを管理しよう！</p>
     </header>
   );
