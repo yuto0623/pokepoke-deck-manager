6a914f578b94cd8377075fa02992db6fd858a678
yuto <yuto.ryr0623@gmail.com>
2025-04-08 20:38:38 +0900

    Refactor Header component to use neumorphism styling

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <header className="bg-blue-500 text-white p-4">
+    <header className="neumorphism p-4">
       <h1 className="text-2xl font-bold">ポケカ デッキビルダー</h1>
       <p className="mt-2">あなたのデッキを管理しよう！</p>
     </header>
